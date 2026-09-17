681f8b94c1b8020b84983504400009b278b61a40
TeeraphatP2 <teerapat200001@gmail.com>
2026-09-17 22:54:00 +0700

    feat: submit exercises 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,6 +4,10 @@ const App = () =>{
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const allValue = good + neutral + bad
+  const average = (good - bad) / allValue
+  const positive = (good / allValue) * 100
   
   return (
     <div>
@@ -15,6 +19,9 @@ const App = () =>{
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {allValue}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
